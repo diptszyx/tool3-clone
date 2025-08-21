449d4a989ce3e7766c4645fec567f8c3de7c1eb1
diptszyx <ndtho2003@@gmail.com>
2025-08-21 15:04:57 +0700

    add available amount sell sol devnet

--- src/components/sell-sol-devnet/receive-sol-devnet.tsx
+++ src/components/sell-sol-devnet/receive-sol-devnet.tsx
@@ -1,77 +1,110 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FormControl, FormField } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { Loader } from "@nsmr/pixelart-react";
 import { UseFormReturn } from "react-hook-form";
 import { FormSellSol } from "./sell-sol-devnet";
+import { Connection, PublicKey } from "@solana/web3.js";
 
 interface ReceiveSolDevnetProps {
-    form: UseFormReturn<FormSellSol>;
-    priceLoading: boolean;
-    isSwapped: boolean;
-    onAmountChange: (value: string) => void;
-    USDPERSOL: number;
+  form: UseFormReturn<FormSellSol>;
+  priceLoading: boolean;
+  isSwapped: boolean;
+  onAmountChange: (value: string) => void;
+  USDPERSOL: number;
 }
 
 const ReceiveSolDevnet: React.FC<ReceiveSolDevnetProps> = ({
-    form,
-    priceLoading,
-    isSwapped,
-    onAmountChange,
-    USDPERSOL,
+  form,
+  priceLoading,
+  isSwapped,
+  onAmountChange,
+  USDPERSOL,
 }) => {
-    return (
-        <div className="bg-white border-gear-gray p-3 flex flex-col min-h-[120px] justify-between pt-[18px]">
-            <div className="flex items-center justify-between mb-2 h-[26px]">
-                <div className="ml-[4px]">You Receive</div>
-                <div className="flex items-center sm:gap-4 gap-1 mr-1">
-                    ${USDPERSOL} per SOL Devnet
-                </div>
-            </div>
-            <div className="flex justify-between items-center mb-2 mt-4 h-[40px] gap-4">
-                <div className="flex gap-2 items-center text-gray-700 border-gear-gray px-2 py-1 ml-2">
-                    <Image
-                        src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
-                        alt="Solana"
-                        width={24}
-                        height={24}
-                        className="rounded-full object-cover"
-                    />
-                    <div className="mt-[2px]">SOL Devnet</div>
-                </div>
-                <div className="w-[200px] sm:w-[300px] h-[40px] flex items-center justify-end">
-                    {priceLoading && !isSwapped ? (
-                        <Loader className="h-6 w-6 animate-spin text-gray-500 mb-1" />
-                    ) : (
-                        <FormField
-                            control={form.control}
-                            name="solAmount"
-                            render={({ field }) => (
-                                <FormControl>
-                                    <Input
-                                        type="number"
-                                        className="focus-visible:ring-0 focus-visible:border-none focus-visible:outline-none outline-none ring-0 border-none shadow-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-right md:!text-[32px] !text-[24px] pr-0"
-                                        placeholder="0.00"
-                                        disabled={!isSwapped}
-                                        {...field}
-                                        onChange={(e) => {
-                                            let value = e.target.value;
-                                            if (parseFloat(value) < 0) value = "0";
-                                            field.onChange(value);
-                                            if (isSwapped) {
-                                                onAmountChange(value);
-                                            }
-                                        }}
-                                    />
-                                </FormControl>
-                            )}
-                        />
-                    )}
-                </div>
-            </div>
+  const [maxSol, setMaxSol] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchBalance = async () => {
+      try {
+        const connection = new Connection("https://api.devnet.solana.com");
+        const adminKey = process.env.NEXT_PUBLIC_ADMIN_PUBLIC_KEY;
+        if (!adminKey) return;
+
+        const balance = await connection.getBalance(new PublicKey(adminKey));
+        setMaxSol(balance / 1_000_000_000);
+      } catch (err) {
+        console.error("Failed to fetch admin SOL balance:", err);
+      }
+    };
+
+    fetchBalance();
+  }, []);
+
+  return (
+    <div className="bg-white border-gear-gray p-3 flex flex-col min-h-[120px] justify-between pt-[18px]">
+      <div className="flex items-center justify-between mb-2 h-[26px]">
+        <div className="ml-[4px]">You Buy</div>
+        <div className="flex flex-col items-end mr-1">
+          <div>${USDPERSOL} per SOL Devnet</div>
+          <div className="text-xs text-gray-500">
+            Max available: {maxSol.toFixed(1)}
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="flex justify-between items-center mb-2 mt-4 h-[40px] gap-4">
+        <div className="flex gap-2 items-center text-gray-700 border-gear-gray px-2 py-1 ml-2">
+          <Image
+            src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
+            alt="Solana"
+            width={24}
+            height={24}
+            className="rounded-full object-cover"
+          />
+          <div className="mt-[2px]">SOL Devnet</div>
+        </div>
+        <div className="w-[200px] sm:w-[300px] h-[40px] flex items-center justify-end">
+          {priceLoading && !isSwapped ? (
+            <Loader className="h-6 w-6 animate-spin text-gray-500 mb-1" />
+          ) : (
+            <FormField
+              control={form.control}
+              name="solAmount"
+              render={({ field }) => (
+                <FormControl>
+                  <Input
+                    type="number"
+                    max={maxSol}
+                    className="focus-visible:ring-0 focus-visible:border-none focus-visible:outline-none outline-none ring-0 border-none shadow-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-right md:!text-[32px] !text-[24px] pr-0"
+                    placeholder="0.00"
+                    disabled={!isSwapped}
+                    {...field}
+                    onChange={(e) => {
+                      let value = e.target.value;
+                      const numValue = parseFloat(value);
+
+                      if (numValue < 0) value = "0";
+
+                      if (numValue > maxSol) {
+                        value = maxSol.toFixed(1);
+                      }
+
+                      field.onChange(value);
+
+                      if (isSwapped) {
+                        onAmountChange(value);
+                      }
+                    }}
+                  />
+                </FormControl>
+              )}
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ReceiveSolDevnet;
--- src/components/sell-sol-devnet/sell-sol-devnet.tsx
+++ src/components/sell-sol-devnet/sell-sol-devnet.tsx
@@ -17,259 +17,280 @@ import { Transaction } from "@solana/web3.js";
 import { UserToken } from "@/hooks/useUserTokens";
 import { NATIVE_SOL } from "@/utils/constants";
 
-
 const formSchema = z.object({
-    amount: z.string(),
-    solAmount: z.string(),
+  amount: z.string(),
+  solAmount: z.string(),
 });
 
 export type FormSellSol = z.infer<typeof formSchema>;
 
 export default function SellSolDevnet() {
-    const isMobile = useIsMobile();
-    const [selectedToken, setSelectedToken] = useState<UserToken | null>(null);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [priceLoading, setPriceLoading] = useState<boolean>(false);
-    const [isSwapped, setIsSwapped] = useState<boolean>(false);
+  const isMobile = useIsMobile();
+  const [selectedToken, setSelectedToken] = useState<UserToken | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [priceLoading, setPriceLoading] = useState<boolean>(false);
+  const [isSwapped, setIsSwapped] = useState<boolean>(true);
 
-    const { publicKey, signTransaction } = useWallet();
+  const { publicKey, signTransaction } = useWallet();
 
-    const USDPERSOL = 0.049;
+  const USDPERSOL = 0.049;
 
-    const form = useForm<FormSellSol>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            amount: "",
-            solAmount: "",
-        },
-    });
+  const form = useForm<FormSellSol>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      amount: "",
+      solAmount: "",
+    },
+  });
 
-    const fetchPrice = useCallback(
-        async (inputAmount: string) => {
-            try {
-                setPriceLoading(true);
-                if (!inputAmount || Number(inputAmount) === 0 || !selectedToken) {
-                    form.setValue(isSwapped ? "amount" : "solAmount", "0.00");
-                    return;
-                }
-
-                let tokenMint = selectedToken?.address || "";
-                if (selectedToken?.address === NATIVE_SOL) {
-                    tokenMint = "So11111111111111111111111111111111111111112";
-                }
-
-                const response = await fetch(`https://lite-api.jup.ag/price/v2?ids=${tokenMint}`);
-
-                const result = await response.json();
-                const priceInUSD = result?.data[tokenMint]?.price;
-
-                if (priceInUSD) {
-                    const amountValue = parseFloat(inputAmount);
-                    if (!isNaN(amountValue) && amountValue > 0) {
-                        if (isSwapped) {
-                            const tokenAmount = (amountValue * USDPERSOL) / priceInUSD;
-                            form.setValue("amount", tokenAmount.toFixed(selectedToken?.decimals || 6));
-                        } else {
-                            const solAmount = (amountValue * priceInUSD) / USDPERSOL;
-                            form.setValue("solAmount", solAmount.toFixed(9));
-                        }
-                    } else {
-                        form.setValue(isSwapped ? "amount" : "solAmount", "");
-                    }
-                } else {
-                    form.setValue(isSwapped ? "amount" : "solAmount", "");
-                    toast.error("Failed to fetch price data");
-                }
-            } catch (e) {
-                console.error("Failed to fetch price data:", e);
-                form.setValue(isSwapped ? "amount" : "solAmount", "");
-                toast.error("Failed to fetch price data");
-            } finally {
-                setPriceLoading(false);
-            }
-        },
-        [selectedToken, form, isSwapped]
-    );
-
-    const debouncedFetchPrice = useMemo(() => {
-        return debounce((amount: string) => {
-            fetchPrice(amount);
-        }, 300);
-    }, [fetchPrice]);
-
-    const handleSwap = () => {
-        setIsSwapped(!isSwapped);
-        form.setValue("amount", "");
-        form.setValue("solAmount", "");
-    };
-
-    const onSubmit = async (values: FormSellSol) => {
-        try {
-            setLoading(true);
-            if (!publicKey || !signTransaction) {
-                toast.error("Please connect your wallet first");
-                return;
-            }
-            if (!selectedToken) {
-                toast.error("Please select a token");
-                return;
-            }
-
-            const tokenAmount = parseFloat(values.amount);
-            const solAmount = parseFloat(values.solAmount);
-
-            if (tokenAmount > parseFloat(selectedToken.balance)) {
-                toast.error(`Insufficient ${selectedToken.symbol} balance`);
-                return;
-            }
-
-            if (tokenAmount <= 0 || Number.isNaN(tokenAmount) || solAmount <= 0 || Number.isNaN(solAmount)) {
-                toast.error("Amount must be greater than 0");
-                return;
-            }
-
-            const sellData = {
-                walletPublicKey: publicKey.toString(),
-                tokenAmount: tokenAmount * Math.pow(10, selectedToken.decimals || 0),
-                tokenMint: selectedToken.address,
-                solAmount: solAmount * 1_000_000_000,
-            };
-
-            const response = await fetch("/api/sell-sol-devnet", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(sellData),
-            });
-
-            const data = await response.json();
-
-            if (!response.ok) {
-                throw new Error(data.error || "Failed to create transaction");
-            }
-
-            const tokenTx = Transaction.from(Buffer.from(data.serializedTx, "base64"));
-            const signedTx = await signTransaction(tokenTx);
-
-            const confirmResponse = await fetch("/api/sell-sol-devnet/confirm", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    signedTransaction: Array.from(signedTx.serialize()),
-                    walletPublicKey: publicKey.toString(),
-                    solAmount: sellData.solAmount,
-                    tokenMint: selectedToken.address,
-                }),
-            });
-
-            const confirmData = await confirmResponse.json();
-
-            if (!confirmResponse.ok) {
-                throw new Error(confirmData.error || "Failed to send SOL Devnet");
-            }
-
-            toast.success("🎉 Buy sol devnet Successful!", {
-                description: `You sold ${values.amount} ${selectedToken.symbol} for ${values.solAmount} SOL Devnet`,
-                action: {
-                    label: "View Transaction",
-                    onClick: () =>
-                        window.open(
-                            `https://solscan.io/tx/${confirmData.solTxSignature}?cluster=devnet`,
-                            "_blank"
-                        ),
-                },
-            });
-            setSelectedToken(null)
-            form.reset();
-        } catch (error) {
-            const message = error instanceof Error ? error.message : "An unexpected error occurred";
-            toast.error(message);
-        } finally {
-            setLoading(false);
+  const fetchPrice = useCallback(
+    async (inputAmount: string) => {
+      try {
+        setPriceLoading(true);
+        if (!inputAmount || Number(inputAmount) === 0 || !selectedToken) {
+          form.setValue(isSwapped ? "amount" : "solAmount", "0.00");
+          return;
         }
-    };
 
-    useEffect(() => {
-        const amount = form.getValues(isSwapped ? "solAmount" : "amount");
-        if (amount) {
-            debouncedFetchPrice(amount);
+        let tokenMint = selectedToken?.address || "";
+        if (selectedToken?.address === NATIVE_SOL) {
+          tokenMint = "So11111111111111111111111111111111111111112";
         }
-    }, [selectedToken, isSwapped, debouncedFetchPrice, form]);
 
-    return (
-        <div className={`md:p-2 max-w-[550px] mx-auto my-2 ${!isMobile && "border-gear"}`}>
-            <h1 className="text-2xl font-bold text-gray-900 mb-8 text-center">Buy SOL Devnet</h1>
-            <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 flex flex-col justify-center">
-                    <div className="px-[5px] space-y-6">
-                        {isSwapped ? (
-                            <ReceiveSolDevnet
-                                form={form}
-                                priceLoading={priceLoading}
-                                isSwapped={isSwapped}
-                                onAmountChange={debouncedFetchPrice}
-                                USDPERSOL={USDPERSOL}
-                            />
-                        ) : (
-                            <SelectToken
-                                title="You Pay"
-                                selectedToken={selectedToken}
-                                setSelectedToken={setSelectedToken}
-                                amount={form.watch("amount")}
-                                onAmountChange={(value) => {
-                                    form.setValue("amount", value);
-                                    debouncedFetchPrice(value);
-                                }}
-                            />
-                        )}
+        const response = await fetch(
+          `https://lite-api.jup.ag/price/v2?ids=${tokenMint}`
+        );
 
-                        <div className="flex justify-center">
-                            <Button
-                                type="button"
-                                variant="outline"
-                                size="icon"
-                                className="border-gear-gray bg-white text-gray-700 hover:bg-gray-100 cursor-pointer"
-                                onClick={handleSwap}
-                            >
-                                <ArrowsVertical className="h-6 w-6" />
-                            </Button>
-                        </div>
+        const result = await response.json();
+        const priceInUSD = result?.data[tokenMint]?.price;
 
-                        {isSwapped ? (
-                            <SelectToken
-                                title="You Pay"
-                                selectedToken={selectedToken}
-                                setSelectedToken={setSelectedToken}
-                                onAmountChange={(value) => {
-                                    form.setValue("amount", value);
-                                }}
-                                disabled={true}
-                                amount={form.watch("amount")}
-                                amountLoading={priceLoading}
-                            />
-                        ) : (
-                            <ReceiveSolDevnet
-                                form={form}
-                                priceLoading={priceLoading}
-                                isSwapped={isSwapped}
-                                onAmountChange={debouncedFetchPrice}
-                                USDPERSOL={USDPERSOL}
-                            />
-                        )}
-                    </div>
-                    <Button
-                        type="submit"
-                        className="w-full text-white font-semibold py-2 rounded-lg transition-colors duration-200 cursor-pointer mt-4"
-                        variant="default"
-                        disabled={loading}
-                    >
-                        {loading ? "Processing..." : "Buy SOL Devnet"}
-                    </Button>
-                </form>
-            </Form>
-        </div>
-    );
+        if (priceInUSD) {
+          const amountValue = parseFloat(inputAmount);
+          if (!isNaN(amountValue) && amountValue > 0) {
+            if (isSwapped) {
+              const tokenAmount = (amountValue * USDPERSOL) / priceInUSD;
+              form.setValue(
+                "amount",
+                tokenAmount.toFixed(selectedToken?.decimals || 6)
+              );
+            } else {
+              const solAmount = (amountValue * priceInUSD) / USDPERSOL;
+              form.setValue("solAmount", solAmount.toFixed(9));
+            }
+          } else {
+            form.setValue(isSwapped ? "amount" : "solAmount", "");
+          }
+        } else {
+          form.setValue(isSwapped ? "amount" : "solAmount", "");
+          toast.error("Failed to fetch price data");
+        }
+      } catch (e) {
+        console.error("Failed to fetch price data:", e);
+        form.setValue(isSwapped ? "amount" : "solAmount", "");
+        toast.error("Failed to fetch price data");
+      } finally {
+        setPriceLoading(false);
+      }
+    },
+    [selectedToken, form, isSwapped]
+  );
+
+  const debouncedFetchPrice = useMemo(() => {
+    return debounce((amount: string) => {
+      fetchPrice(amount);
+    }, 300);
+  }, [fetchPrice]);
+
+  const handleSwap = () => {
+    setIsSwapped(!isSwapped);
+    form.setValue("amount", "");
+    form.setValue("solAmount", "");
+  };
+
+  const onSubmit = async (values: FormSellSol) => {
+    try {
+      setLoading(true);
+      if (!publicKey || !signTransaction) {
+        toast.error("Please connect your wallet first");
+        return;
+      }
+      if (!selectedToken) {
+        toast.error("Please select a token");
+        return;
+      }
+
+      const tokenAmount = parseFloat(values.amount);
+      const solAmount = parseFloat(values.solAmount);
+
+      if (tokenAmount > parseFloat(selectedToken.balance)) {
+        toast.error(`Insufficient ${selectedToken.symbol} balance`);
+        return;
+      }
+
+      if (
+        tokenAmount <= 0 ||
+        Number.isNaN(tokenAmount) ||
+        solAmount <= 0 ||
+        Number.isNaN(solAmount)
+      ) {
+        toast.error("Amount must be greater than 0");
+        return;
+      }
+
+      const sellData = {
+        walletPublicKey: publicKey.toString(),
+        tokenAmount: tokenAmount * Math.pow(10, selectedToken.decimals || 0),
+        tokenMint: selectedToken.address,
+        solAmount: solAmount * 1_000_000_000,
+      };
+
+      const response = await fetch("/api/sell-sol-devnet", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(sellData),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error || "Failed to create transaction");
+      }
+
+      const tokenTx = Transaction.from(
+        Buffer.from(data.serializedTx, "base64")
+      );
+      const signedTx = await signTransaction(tokenTx);
+
+      const confirmResponse = await fetch("/api/sell-sol-devnet/confirm", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          signedTransaction: Array.from(signedTx.serialize()),
+          walletPublicKey: publicKey.toString(),
+          solAmount: sellData.solAmount,
+          tokenMint: selectedToken.address,
+        }),
+      });
+
+      const confirmData = await confirmResponse.json();
+
+      if (!confirmResponse.ok) {
+        throw new Error(confirmData.error || "Failed to send SOL Devnet");
+      }
+
+      toast.success("🎉 Buy sol devnet Successful!", {
+        description: `You sold ${values.amount} ${selectedToken.symbol} for ${values.solAmount} SOL Devnet`,
+        action: {
+          label: "View Transaction",
+          onClick: () =>
+            window.open(
+              `https://solscan.io/tx/${confirmData.solTxSignature}?cluster=devnet`,
+              "_blank"
+            ),
+        },
+      });
+      setSelectedToken(null);
+      form.reset();
+    } catch (error) {
+      const message =
+        error instanceof Error ? error.message : "An unexpected error occurred";
+      toast.error(message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const amount = form.getValues(isSwapped ? "solAmount" : "amount");
+    if (amount) {
+      debouncedFetchPrice(amount);
+    }
+  }, [selectedToken, isSwapped, debouncedFetchPrice, form]);
+
+  return (
+    <div
+      className={`md:p-2 max-w-[550px] mx-auto my-2 ${
+        !isMobile && "border-gear"
+      }`}
+    >
+      <h1 className="text-2xl font-bold text-gray-900 mb-8 text-center">
+        Buy SOL Devnet
+      </h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-4 flex flex-col justify-center"
+        >
+          <div className="px-[5px] space-y-6">
+            {isSwapped ? (
+              <ReceiveSolDevnet
+                form={form}
+                priceLoading={priceLoading}
+                isSwapped={isSwapped}
+                onAmountChange={debouncedFetchPrice}
+                USDPERSOL={USDPERSOL}
+              />
+            ) : (
+              <SelectToken
+                title="You Pay"
+                selectedToken={selectedToken}
+                setSelectedToken={setSelectedToken}
+                amount={form.watch("amount")}
+                onAmountChange={(value) => {
+                  form.setValue("amount", value);
+                  debouncedFetchPrice(value);
+                }}
+              />
+            )}
+
+            <div className="flex justify-center">
+              <Button
+                type="button"
+                variant="outline"
+                size="icon"
+                className="border-gear-gray bg-white text-gray-700 hover:bg-gray-100 cursor-pointer"
+                onClick={handleSwap}
+              >
+                <ArrowsVertical className="h-6 w-6" />
+              </Button>
+            </div>
+
+            {isSwapped ? (
+              <SelectToken
+                title="You Pay"
+                selectedToken={selectedToken}
+                setSelectedToken={setSelectedToken}
+                onAmountChange={(value) => {
+                  form.setValue("amount", value);
+                }}
+                disabled={true}
+                amount={form.watch("amount")}
+                amountLoading={priceLoading}
+              />
+            ) : (
+              <ReceiveSolDevnet
+                form={form}
+                priceLoading={priceLoading}
+                isSwapped={isSwapped}
+                onAmountChange={debouncedFetchPrice}
+                USDPERSOL={USDPERSOL}
+              />
+            )}
+          </div>
+          <Button
+            type="submit"
+            className="w-full text-white font-semibold py-2 rounded-lg transition-colors duration-200 cursor-pointer mt-4"
+            variant="default"
+            disabled={loading}
+          >
+            {loading ? "Processing..." : "Buy SOL Devnet"}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
