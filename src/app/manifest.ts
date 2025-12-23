89187f5a9c29498fbe4116548f51bdc86823a7e2
diptszyx <ndtho2003@@gmail.com>
2025-12-23 17:03:51 +0700

    1

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -28,6 +28,24 @@ export default function manifest(): MetadataRoute.Manifest {
     ],
     icons: [
       {
+        src: '/icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
         src: '/icon-144x144.png',
         sizes: '144x144',
         type: 'image/png',
@@ -44,6 +62,18 @@ export default function manifest(): MetadataRoute.Manifest {
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any',
+      },
+      {
+        src: '/icon-192x192-maskable.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon-512x512-maskable.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
       },
     ],
     categories: ['finance', 'utilities', 'productivity'],
